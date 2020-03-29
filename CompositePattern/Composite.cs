6fda841a1f8cc235f6973da94119e980e70bf28a
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-03-29 19:46:12 +0200

    Add project files.

---
+++ CompositePattern/Composite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Composite:Component
+    {
+        private List<Component> children = new List<Component>();
+
+        public Composite(string name) :base(name)
+        {
+
+        }
+
+         public override void Add(Component component)
+         {
+            children.Add(component);
+         }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new string('-', depth) + name);
+
+
+            //Recursively Display Child Nodes
+            foreach (Component component in children)
+            {
+                component.Display(depth + 2);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ CompositePattern/Leaf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Leaf:Component
+    {
+
+        public Leaf(string name):base(name)
+        {
+
+        }
+
+        public override void Add(Component c)
+        {
+            Console.WriteLine("Cannot Add to a leaf");
+        }
+
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("Cannot Remove from Leaf");
+        }
+
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new string();
+
+            string a = "SD":
+            a.Substring()
+        }
+    }
+}
